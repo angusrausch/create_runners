6528fdfd2dcd9cf5c68fa243dd38b4e80e919419
Angus <angus@rausch.com.au>
2025-06-03 23:39:35 +1000

    Updated autoscaler to work without docker
    added stop option to scaler to remove runners

--- autoscaler.py
+++ autoscaler.py
@@ -1,15 +1,16 @@
 import os
+import shutil
 import time
 from dotenv import load_dotenv
 import requests
 import re
-from distutils.version import LooseVersion
 import platform
 from urllib.request import urlretrieve
 import subprocess
 import tarfile
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 import stat
+import uuid
 
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -19,7 +20,7 @@ POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", 30))
 MIN_RUNNERS = int(os.getenv("Min_RUNNERS", 0))
 MAX_RUNNERS = int(os.getenv("MAX_RUNNERS", 2))
 RUNNERS_DIR = os.getenv("RUNNERS_DIR", "runners")
-DOWNLOADS_DIR = os.getenv("DOWNLOADS_DIR", "downloads")
+DOWNLOADS_DIR = os.getenv("DOWNLOADS_DIR", "download")
 
 if not GITHUB_TOKEN or not REPO_OWNER or not REPO_NAME:
     raise ValueError("Missing env vars: GITHUB_TOKEN, REPO_OWNER, REPO_NAME")
@@ -46,7 +47,6 @@ class Runner_Watcher:
         except FileExistsError:
             pass
         
-
         self.check_updated_runner()
         self.build_runner()
         self.monitor_queued_runs()
@@ -65,22 +65,34 @@ class Runner_Watcher:
         print(f"Monitoring queued GitHub Actions runs for {REPO_OWNER}/{REPO_NAME} every {POLL_INTERVAL}s...\n")
         seen_run_ids = set()
 
-        while True:
-            try:
-                self.get_runs_by_status("queued")
+        try:
+            while True:
+                try:
+                    self.get_runs_by_status("queued")
 
-                if self.queued_runs:
-                    print(f"[{time.strftime('%H:%M:%S')}] Queued runs: {len(self.queued_runs)}")
-                    for run in self.queued_runs:
-                        print(f"  - ID: {run['id']} | Branch: {run['head_branch']} | Event: {run['event']} | Created: {run['created_at']}")
-                        seen_run_ids.add(run["id"])
-                else:
-                    print(f"[{time.strftime('%H:%M:%S')}] No new queued runs.")
+                    if self.queued_jobs:
+                        print(f"[{time.strftime('%H:%M:%S')}] Queued runs: {len(self.queued_jobs)}")
+                        for run in self.queued_jobs:
+                            print(f"  - ID: {run['id']} | Branch: {run['head_branch']} | Event: {run['event']} | Created: {run['created_at']}")
+                            seen_run_ids.add(run["id"])
+                    else:
+                        print(f"[{time.strftime('%H:%M:%S')}] No new queued runs.")
 
-            except Exception as e:
-                print(f"[ERROR] {e}")
+                except Exception as e:
+                    print(f"[ERROR] {e}")
 
-            time.sleep(POLL_INTERVAL)
+                time.sleep(POLL_INTERVAL)
+        except KeyboardInterrupt:
+            print("\nPlease wait while runners are stopped and removed")
+            time.sleep(5)
+            for runner in self.runners:
+                self.stop_runner(runner)
+        except Exception as e:
+            print("Unhandled exception occured:\n{e}\nPlease wait while runners are stopped")
+            for runner in self.runners:
+                self.stop_runner(runner)
+                
+
 
     def get_runner_registration_token(self):
         headers = {
@@ -104,11 +116,8 @@ class Runner_Watcher:
     def build_runner(self, number=1):
         archive_path = self.check_updated_runner()
 
-        offset = 0
         for i in range(number):
-            while len(self.runners) + offset in self.runners:
-                offset += 1
-            runner_name = f"runner_{len(self.runners) + offset}"
+            runner_name = f"runner_{uuid.uuid4().hex}"
             runner_dir = os.path.join(RUNNERS_DIR, runner_name)
             with tarfile.open(archive_path, "r:gz") as tar:
                 tar.extractall(path=runner_dir)
@@ -123,10 +132,10 @@ class Runner_Watcher:
 
     def configure_runner(self, runner_name, runner_dir):
         config_script = os.path.join(runner_dir, "config.sh")
-
         os.chmod(config_script, os.stat(config_script).st_mode | stat.S_IEXEC)
+
         subprocess.run(
-            [config_script, "--url", self.repo_url, "--token", self.token["token"], "--unattended"],
+            [config_script, "--url", self.repo_url, "--token", self.token["token"], "--name", runner_name, "--unattended"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
             check=True,
@@ -134,44 +143,79 @@ class Runner_Watcher:
         )
 
     def start_runner(self, runner_dir):
-        run_script = os.path.join(runner_dir, "run.sh")
+        original_dir = os.getcwd()
+        os.chdir(runner_dir)
 
+        result = subprocess.run(
+            ["sudo", "./svc.sh", "install"],
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=10
+        )
+        result = subprocess.run(
+            ["sudo", "./svc.sh", "start"],
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=10
+        )
+        os.chdir(original_dir)
+
+    def stop_runner(self, runner_dir):
+        original_dir = os.getcwd()
+        os.chdir(runner_dir)
+
+        subprocess.run(
+            ["sudo", "./svc.sh", "stop"],
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=10
+        )
+        subprocess.run(
+            ["sudo", "./svc.sh", "uninstall"],
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=10
+        )
+        subprocess.run(
+            ["./config.sh", "remove", "--token", self.token["token"]],
+            capture_output=True,
+            text=True,
+            check=True,
+            timeout=10
+        )
+        
+        os.chdir(original_dir)
         try:
-            self.runner_process = subprocess.Popen(
-                [run_script],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True
-            )
-
-            print(f"Runner started with PID {self.runner_process.pid}")
-
+            shutil.rmtree(runner_dir)
         except Exception as e:
-            print("Failed to start runner!")
-            print("Error:", str(e))
-
+            print(f"Failed to remove {runner_dir} directory")
 
 
     def check_token(self):
         if self.token:
             expires_at = datetime.strptime(self.token["expires_at"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
             now = datetime.now(timezone.utc)
-            if now <= expires_at:
+            if now + timedelta(minutes=5) <= expires_at:
                 return
-        
         self.get_runner_registration_token()
 
     def check_updated_runner(self):
+        print("Checking current runner version")
         version = Runner_Watcher.find_latest_version()
         arch = Runner_Watcher.find_machine_arch()
 
         filename = f"actions-runner-linux-{arch}-{version}.tar.gz"
         file_path = os.path.join(DOWNLOADS_DIR, filename)
         if os.path.isfile(file_path):
-            print("Contains latest version")
+            print("Runner up to date")
         else:
-            print("Update required")
+            print("Runner update required")
             Runner_Watcher.update_runner(version, filename, file_path)
+            print("Downloaded updated runner")
         return file_path
 
     @classmethod
@@ -197,7 +241,7 @@ class Runner_Watcher:
             print("No versions found.")
             exit(1)
         
-        latest_version = sorted(versions, key=LooseVersion)[-1]
+        latest_version = sorted(versions)[-1]
         return latest_version
 
     @classmethod
@@ -214,5 +258,4 @@ class Runner_Watcher:
 
 
 if __name__ == "__main__":
-    subprocess.Popen(["sudo", "dockerd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     Runner_Watcher()
